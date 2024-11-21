e6c74c3190e73c53b8a85bc27377a98400d4ac98
fanelob46 <fanelob46@gmail.com>
2024-11-21 16:54:31 +0200

    authantication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import express from "express";
 import { registerUser } from "../controllers/users/register.js";
 import { loginUser } from "../controllers/users/login.js";
 import getUserProfile from "../controllers/users/getUserProfile.js";
 import { updateUserProfile } from "../controllers/users/update.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -11,9 +11,9 @@ router.post("/", registerUser);
 router.post("/login", loginUser);
 
 //Update profile
-router.put('/profile', updateUserProfile);
+router.put('/profile',protect, updateUserProfile);
 
 
-router.get("/profile", getUserProfile);
+router.get("/profile",protect, getUserProfile);
 export default router;
 
