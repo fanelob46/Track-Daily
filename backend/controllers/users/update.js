7ed6e8d5e06ced9a054aa844b3242b813e2c904e
noluzar <noluthandosamantha.ns@gmail.com>
2024-11-21 15:44:43 +0200

    commit

---
+++ backend/controllers/users/update.js
@@ -0,0 +1,33 @@
+import asyncHandler from "express-async-handler";
+import User from "../../models/userModel.js";
+import router from "../../routes/userRoutes.js";
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const {id} = req.body
+    const user = await User.findById(id);
+
+    if (user) {
+        user.firstName = req.body.firstName || user.firstName;
+        user.lastName = req.body.lastName || user.lastName;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password){
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id, 
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastNamestName,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+});
+
+
+export { updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express"
 import { registerUser } from "../controllers/users/register.js";
+import { updateUserProfile } from "../controllers/users/update.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
+
+//Update profile
+router.put('/profile', updateUserProfile);
+
 
 export default router;
