d011cf9ef8f63bc5c8113939270aba091873e8f1
fanelob46 <fanelob46@gmail.com>
2024-11-21 14:26:33 +0200

    Register user endpoint

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userShema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userShema);
+
+export default User;
