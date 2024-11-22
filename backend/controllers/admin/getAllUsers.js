9aee8c6049431e05005cc9583750c89e8063c4b6
noluzar <noluthandosamantha.ns@gmail.com>
2024-11-22 15:40:11 +0200

    commit

--- backend/controllers/admin/getAllUsers.js
+++ backend/controllers/admin/getAllUsers.js
@@ -1,1 +1,13 @@
-export const getAllUsers = (req, res) => {};
+import mongoose from "mongoose";
+import User from "../../models/userModel.js";
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        console.log("error in fetching users:", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+};
