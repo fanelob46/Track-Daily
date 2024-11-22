dbe11ba652e55b28c78b1ffe31e6967a58e4d0cb
noluzar <noluthandosamantha.ns@gmail.com>
2024-11-22 12:01:47 +0200

    commit

--- backend/models/tasksModel.js
+++ backend/models/tasksModel.js
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
     title:{
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-app.use('/api/tasks', tasksRoutes);
+app.use("/api/tasks", tasksRoutes);
 
 app.listen(5000, () => {
   connectDB();
