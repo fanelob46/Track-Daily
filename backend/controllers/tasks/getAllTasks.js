d56258424758b5b9b29f1e2513065e5523a22c30
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-22 11:14:42 +0200

    Get all tasks

--- backend/controllers/tasks/getAllTasks.js
+++ backend/controllers/tasks/getAllTasks.js
@@ -1,5 +1,18 @@
-
+import Task from "../../models/tasksModel.js";
 
 export const getAllTasks = async (req, res) => {
-    
+  try {
+    const tasks = await Task.find({});
+    console.log("tasks ", tasks);
+    if (!tasks.length) {
+      return res
+        .status(200)
+        .json({ success: true, message: "No available tasks", data: tasks });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "Successfully fetched tasks", data: tasks });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 };
