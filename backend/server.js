816d05da13e33cb05c01632001a5b4d4d2bef124
noluzar <noluthandosamantha.ns@gmail.com>
2024-11-22 10:45:04 +0200

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import tasksRoutes from './routes/tasksRoutes.js';
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use('/api/tasks', tasksRoutes);
 
 app.listen(5000, () => {
   connectDB();
