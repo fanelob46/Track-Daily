2a5f2b72870ca4fea81c337b7e194d456e1069af
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-25 16:15:06 +0200

    Auth Form, Auth layout and Sign up page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import AddTaskUI from "./components/AddTaskUI";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <>
       <h1 className="text-9xl"> Baloyi fanelo</h1>
       <AddTaskUI />
+      <SignUpPage />
     </>
   );
 }
---
+++ frontend/src/components/AuthForm.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+type AuthFormProps = {
+  name: string;
+  type: "signup" | "login" | "edit";
+  submitFunction: () => void;
+};
+
+const AuthForm = ({ name, type, submitFunction }: AuthFormProps) => {
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleEditToggle = () => {
+    if (type === "edit") {
+      setIsEditable((prev) => !prev);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={submitFunction}
+      className="flex border flex-col px-10 space-y-4"
+    >
+      <h1 className="text-5xl my-8 text-center">{name}</h1>
+
+      {(type === "signup" || type === "edit") && (
+        <div className="flex justify-between border border-red-400">
+          <input
+            type="text"
+            id="firstName"
+            className="inputStyles w-[48%]"
+            placeholder="First Name"
+            disabled={type === "edit" && !isEditable}
+          />
+
+          <input
+            type="text"
+            id="lastName"
+            className="inputStyles w-[48%]"
+            placeholder="Last Name"
+            disabled={type === "edit" && !isEditable}
+          />
+        </div>
+      )}
+
+      <input
+        type="email"
+        id="email"
+        placeholder="Email Address"
+        disabled={type === "edit" && !isEditable}
+      />
+
+      <input
+        type="password"
+        id="password"
+        placeholder="Enter your password"
+        disabled={type === "edit" && !isEditable}
+      />
+
+      {(type === "signup" || type === "edit") && (
+        <>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm your password"
+            disabled={type === "edit" && !isEditable}
+          />
+        </>
+      )}
+
+      <button
+        type={type === "edit" ? "button" : "submit"}
+        onClick={type === "edit" ? handleEditToggle : undefined}
+      >
+        {type === "signup"
+          ? "Sign-Up"
+          : type === "login"
+          ? "Login"
+          : isEditable
+          ? "Save"
+          : "Edit"}
+      </button>
+    </form>
+  );
+};
+
+export default AuthForm;
