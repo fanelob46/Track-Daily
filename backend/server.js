d2e6bab947453588ada332899322648767c120d8
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-29 14:17:18 +0200

    server update - cors added

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,26 @@ import { protect } from "./middleware/authMiddleware.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://track-daily-rouge.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
 app.use(express.json());
-
 app.use(cookieParser());
-
-app.use(cors({ origin: "https://track-daily-rouge.vercel.app/" }));
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", protect, tasksRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
 
 export default app;
